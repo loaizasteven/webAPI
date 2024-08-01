4dd11d68e86b6e86082cdbac04670ac946322533
Steven Loaiza <steven.loaiza@outlook.com>
2024-08-01 12:21:42 -0400

    Refactor and add gettoken method

--- localDevAPI/main.py
+++ localDevAPI/main.py
@@ -18,11 +18,13 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/getToken/")
+async def get_token(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
 
 @app.get("/items/{item_id}")
-def read_item(token: Annotated[str, Depends(oauth2_scheme)], item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
