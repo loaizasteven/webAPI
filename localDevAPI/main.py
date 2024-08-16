57848a8ab295318fc4e0ae8c7042e5b04878fc40
Steven Loaiza <steven.loaiza@outlook.com>
2024-08-16 00:07:07 -0400

    Hash and verify password methods

--- localDevAPI/main.py
+++ localDevAPI/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
+from passlib.context import CryptContext
+
 #Example of accessing the secrets within the container
 secret_path = 'run/secrets/oauth_token'
 
@@ -54,8 +56,23 @@ def get_user(db, username:str):
         user_dict = db.get(username)
         return UserInDB(**user_dict)
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
 # Oauth
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI(
     title="Web Application with Authentication",
