1db61404ae5a331a4c7287fa83d03512e20c746e
Steven Loaiza <steven.loaiza@outlook.com>
2024-08-16 00:17:48 -0400

    Update get_current_user method

--- localDevAPI/main.py
+++ localDevAPI/main.py
@@ -12,7 +12,7 @@ from jwt.exceptions import InvalidTokenError
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = ""
+SECRET_KEY = "487b25a85cf41c09fff6710395e585f2c0521700374917e232bf3bf9f8fec870"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -46,6 +46,10 @@ def fake_hash_password(password: str):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
 
 
 class Item(BaseModel):
@@ -129,15 +133,23 @@ def fake_decode_token(token):
     return get_user(fake_users_db, token)
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        # Any HTTP 401 Unauthorized needs to return this header as per specs.
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    return user
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate crendentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user 
 
 async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)],
