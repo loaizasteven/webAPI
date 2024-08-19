3a3518f5ddf41aea923afd8cbba0045b2cb58381
Steven Loaiza <steven.loaiza@outlook.com>
2024-08-19 17:06:06 -0400

    Remove unused methods in APP

--- localDevAPI/main.py
+++ localDevAPI/main.py
@@ -10,6 +10,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 file_dir = os.path.dirname(__file__)
 root_dir = os.path.dirname(file_dir)
 sys.path.insert(0, root_dir)
+
 from security.classstructures import Token, Item, User, UserInDB
 from security.authentication import *
 from security import ACCESS_TOKEN_EXPIRE_MINUTES
@@ -29,10 +30,6 @@ app = FastAPI(
 @app.get("/")
 def read_roots():
     return {"message": "This app is serving locally @ localhost/docs"}
-
-@app.get("/getToken/")
-async def get_token(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
@@ -58,18 +55,6 @@ async def login_for_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
-
-@app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
 
 @app.get("/users/me", response_model=User)
 async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
