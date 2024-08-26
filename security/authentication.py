9be642d1959fcd2a17d8ce993f5268eb507ba75a
Steven Loaiza <steven.loaiza@outlook.com>
2024-08-25 23:30:36 -0400

    remove unused method

--- security/authentication.py
+++ security/authentication.py
@@ -54,11 +54,6 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None, defa
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-
-def fake_decode_token(token:Any, user_db:Dict = USERDATABASE):
-    """This does not provide any security yet"""
-    return get_user(user_db, token)
-
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user_db = USERDATABASE
     exceptions_ = ErrorRaise()
